d2902333ee5e10ee4812f49eb77f328759fa444c
Carlos <cbiosca82@gmail.com>
2025-06-10 23:38:16 +0200

    connect screen for desktop

--- frontend/src/components/Connect/Connect.jsx
+++ frontend/src/components/Connect/Connect.jsx
@@ -6,34 +6,48 @@ import "./Connect.css";
 const Connect = () => {
   return (
     <main className="connect">
-      <Title
-        title="Connect Your Account"
-        subtitle="...and unlock your benefits!"
-      />
-      <Benefits />
-      <form action="" className="email">
-        <input
-          type="email"
-          placeholder="Email Address"
-          className="email__input"
+      <section>
+        <Title
+          title="Connect Your Account"
+          subtitle="...and unlock your benefits!"
+          classes="only-mobile"
         />
-        <label htmlFor="offers" className="email__label">
+        <Benefits />
+        <small className="connect__terms only-desktop">
+          By continuing, you agree to our <span>Terms & Conditions</span> and{" "}
+          <span>Privacy Policy</span>.
+        </small>
+      </section>
+      <section>
+        <Title
+          title="Connect Your Account"
+          subtitle="...and unlock your benefits!"
+          classes="only-desktop"
+        />
+        <form action="" className="email">
           <input
-            type="checkbox"
-            name="offers"
-            id="offers"
-            className="email__checkbox"
+            type="email"
+            placeholder="Email Address"
+            className="email__input"
           />
-          Send Me Offers, News, and Fun Stuff!
-        </label>
-        <button type="submit" className="email__button">
-          Connect
-        </button>
-      </form>
-      <small className="connect__terms">
-        By continuing, you agree to our <span>Terms & Conditions</span> and{" "}
-        <span>Privacy Policy</span>.
-      </small>
+          <label htmlFor="offers" className="email__label">
+            <input
+              type="checkbox"
+              name="offers"
+              id="offers"
+              className="email__checkbox"
+            />
+            Send Me Offers, News, and Fun Stuff!
+          </label>
+          <button type="submit" className="email__button">
+            Connect
+          </button>
+        </form>
+        <small className="connect__terms only-mobile">
+          By continuing, you agree to our <span>Terms & Conditions</span> and{" "}
+          <span>Privacy Policy</span>.
+        </small>
+      </section>
     </main>
   );
 };
--- frontend/src/components/common/Title/Title.jsx
+++ frontend/src/components/common/Title/Title.jsx
@@ -1,11 +1,11 @@
 import "./Title.css";
 
-const Title = ({ title, subtitle }) => {
+const Title = ({ title, subtitle, classes }) => {
   return (
-    <>
+    <div className={classes}>
       <h1 className="title">{title}</h1>
       {subtitle && <p className="subtitle">{subtitle}</p>}
-    </>
+    </div>
   );
 };
 

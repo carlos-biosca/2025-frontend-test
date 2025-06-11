8d03aad4128971238eb546a73fc0a36054bdffe0
Carlos <cbiosca82@gmail.com>
2025-06-11 18:05:20 +0200

    title email prop

--- frontend/src/components/common/Title/Title.jsx
+++ frontend/src/components/common/Title/Title.jsx
@@ -1,10 +1,11 @@
 import "./Title.css";
 
-const Title = ({ title, subtitle, classes }) => {
+const Title = ({ title, subtitle, classes, email }) => {
   return (
-    <div className={classes}>
+    <div className={`title__container ${classes}`}>
       <h1 className="title">{title}</h1>
       {subtitle && <p className="subtitle">{subtitle}</p>}
+      {email && <p className="title__email">{email}</p>}
     </div>
   );
 };

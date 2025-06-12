02dd7a390da17de472c411072dee8d711080bf50
Carlos <cbiosca82@gmail.com>
2025-06-12 23:39:15 +0200

    plan options component

---
+++ frontend/src/components/common/PlanOptions/PlanOptions.jsx
@@ -0,0 +1,80 @@
+import plancheck from "@assets/plan-check.svg";
+
+import "./PlanOptions.css";
+
+const PlanOptions = ({ plan, handlePlanChange }) => {
+  return (
+    <>
+      <div className="plan__discount">Save 20%</div>
+      <label htmlFor="yearly" className="plan">
+        <input
+          type="radio"
+          name="plan"
+          id="yearly"
+          value="yearly"
+          onChange={handlePlanChange}
+          className="plan__input"
+        />
+
+        <div className="plan__time">
+          <div
+            className={`plan__circle ${
+              plan === "yearly" ? "plan__circle--active" : "plan__circle--empty"
+            }`}
+          >
+            <img src={plancheck} alt="check" className="plan__icon" />
+          </div>
+          <div>
+            <p className="plan__best">BEST VALUE</p>
+            <h2 className="plan__title">Annual</h2>
+          </div>
+        </div>
+        <div className="plan__price">
+          <div className="plan__value">
+            <p className="plan__money">
+              <span>$ 89.99</span> /year
+            </p>
+            <p className="plan__annually">Billed annually</p>
+          </div>
+          <p className="plan__trial">7-day free trial</p>
+        </div>
+      </label>
+
+      <label htmlFor="monthly" className="plan">
+        <input
+          type="radio"
+          name="plan"
+          id="monthly"
+          value="monthly"
+          onChange={handlePlanChange}
+          className="plan__input"
+        />
+        <div className="plan__time">
+          <div
+            className={`plan__circle ${
+              plan === "monthly"
+                ? "plan__circle--active"
+                : "plan__circle--empty"
+            }`}
+          >
+            <img src={plancheck} alt="check" className="plan__icon" />
+          </div>
+          <div>
+            <h2 className="plan__title">Monthly</h2>
+          </div>
+        </div>
+        <div className="plan__price">
+          <div className="plan__value">
+            <p className="plan__money">
+              <span>$ 9.99</span> /year
+            </p>
+            <p className="plan__annually">Billed monthly</p>
+          </div>
+          <p className="plan__trial">3-day free trial</p>
+        </div>
+      </label>
+    </>
+  );
+};
+
+export default PlanOptions;

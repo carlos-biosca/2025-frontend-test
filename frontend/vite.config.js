a6eebe0ca1495acb49eb472b6f6080ac989cbaea
Carlos <cbiosca82@gmail.com>
2025-08-22 21:00:21 +0200

    frontend vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,36 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@logic': '/src/logic',
-      '@context': '/src/context',
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname, '')
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_API_URL__: JSON.stringify(env.VITE_API_URL),
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@assets': '/src/assets',
+        '@logic': '/src/logic',
+        '@context': '/src/context',
+      },
+    },
+    base: './',
+  }
 })

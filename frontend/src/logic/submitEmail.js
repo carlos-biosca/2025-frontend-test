59914ad96690325bc92b799a330125df0affa5d8
Carlos <cbiosca82@gmail.com>
2025-08-22 20:30:23 +0200

    refactor submit email as POST

--- frontend/src/logic/submitEmail.js
+++ frontend/src/logic/submitEmail.js
@@ -1,10 +1,11 @@
 const submitEmail = async (email) => {
   try {
-    const res = await fetch(`/api/send-email?email=${email}`, {
-      method: "GET",
+    const res = await fetch("/api/send-email", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json"
-      }
+      },
+      body: JSON.stringify({ email })
     });
     const data = await res.json();
     return data
